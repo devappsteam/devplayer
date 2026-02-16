08645d6cca55e3a672dedede75cdfa4cf42a68b6
Caio Felipe <caio@devapps.com.br>
2026-02-16 14:36:46 -0300

    feat: No index e health

--- devplayer_api/bootstrap/app.php
+++ devplayer_api/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
+        $middleware->append(\App\Http\Middleware\NoIndexHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- devplayer_api/routes/web.php
+++ devplayer_api/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'service' => config('app.name', 'DevPlayer API'),
+        'version' => app()->version(),
+        'time' => now()->toIso8601String(),
+    ]);
+});
